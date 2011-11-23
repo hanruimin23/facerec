2772226401be6a64b1c0819935f8f79be3a3364b
Rafael Barreto <rafaelbarreto87@gmail.com>
2011-11-23 16:22:06 -0300

    interface for using validation sets in PairsDataset class

--- src/+datasets/PairsDataset.m
+++ src/+datasets/PairsDataset.m
@@ -74,23 +74,51 @@ classdef PairsDataset < matlab.mixin.Copyable
             obj.pairs = [obj.pairs; pairs];
         end
 
-        function [training, test] = getFold(obj, fold)
+        function [training, val, test] = getFold(obj, fold, trainToValRatio)
             assert(fold >= 1 && fold <= obj.NumberOfFolds, sprintf( ...
                 'There''s no fold ''%d''.', fold));
+
+            if nargin < 3
+                trainToValRatio = 1;
+            end
+
+            assert(trainToValRatio > 0 && trainToValRatio <= 1, ...
+                '''trainToValRatio'' must be in (0, 1] interval.');
 
             mask = cell2mat(obj.pairs(:, end)) == fold;
 
             training = obj.samplesData.values(obj.pairs(~mask, 1:2));
             training = [training, obj.pairs(~mask, 3)];
+            nTraining = ceil(trainToValRatio * size(training, 1));
+            val = training(nTraining + 1:end, :);
+            training = training(1:nTraining, :);
 
             test = obj.samplesData.values(obj.pairs(mask, 1:2));
             test = [test, obj.pairs(mask, 3)];
         end
 
-        function stats = evalMatcher(obj, fold, scoreFun)
-            [~, test] = obj.getFold(fold);
+        function [testStats, valStats, trainingStats] = evalMatcher(obj, ...
+                fold, scoreFun, trainToValRatio)
+            if nargin < 4
+                trainToValRatio = 1;
+            end
+
+            [training, val, test] = obj.getFold(fold, trainToValRatio);
             scores = utils.computeScores(test(:, 1), test(:, 2), scoreFun);
-            stats = utils.Stats(scores, cell2mat(test(:, 3)));
+            testStats = utils.Stats(scores, cell2mat(test(:, 3)));
+            if nargout >= 2
+                valStats = [];
+                if ~isempty(val)
+                    scores = utils.computeScores(val(:, 1), val(:, 2), ...
+                        scoreFun);
+                    valStats = utils.Stats(scores, cell2mat(val(:, 3)));
+                end
+            end
+            if nargout == 3
+                scores = utils.computeScores(training(:, 1), training(:, 2), ...
+                    scoreFun);
+                trainingStats = utils.Stats(scores, cell2mat(training(:, 3)));
+            end
         end
 
         function newObj = apply(obj, fun, newCopy)
