2e4538a189efaf4814b9cd6604c49e736273bea0
Rafael Barreto <rafaelbarreto87@gmail.com>
2011-11-23 16:21:25 -0300

    minor changes in LBP and lfw.preprocess functions

--- src/+datasets/+lfw/preprocess.m
+++ src/+datasets/+lfw/preprocess.m
@@ -1,13 +1,12 @@
-function out = preprocess(I, m, n, downsamplingFactor)
+function out = preprocess(I, faceSize, downsamplingFactor)
 [M, N, P] = size(I);
 if P == 3
     I = rgb2gray(I);
 end
 
-y0 = floor((M - m) / 2);
-y1 = y0 + m - 1;
-x0 = floor((N - n) / 2);
-x1 = x0 + n - 1;
-
-out = im2double(I(y0:y1, x0:x1));
+y0 = floor((M - faceSize(1)) / 2);
+y1 = y0 + faceSize(1) - 1;
+x0 = floor((N - faceSize(2)) / 2);
+x1 = x0 + faceSize(2) - 1;
+out = imadjust(wiener2(im2double(I(y0:y1, x0:x1))));
 out = imresize(out, downsamplingFactor);
--- src/+feats/lbp.m
+++ src/+feats/lbp.m
@@ -9,6 +9,15 @@ if nargin < 3
     radius = 1;
 end
 
-out = blockproc(I, [blkSize, blkSize], @(blkStruct) lbp.lbp(blkStruct.data, ...
-    radius, 8, lbpMapping, 'nh'), 'PadPartialBlocks', true);
+[M, N] = size(I);
+out = zeros(ceil(M / blkSize(1)) * ceil(N / blkSize(2)), lbpMapping.num);
+n = 1;
+for y0 = 1:blkSize(1):M
+    for x0 = 1:blkSize(2):N
+        y1 = min(y0 + blkSize(1) - 1, M);
+        x1 = min(x0 + blkSize(2) - 1, N);
+        out(n, :) = lbp.lbp(I(y0:y1, x0:x1), radius, 8, lbpMapping, 'h');
+        n = n + 1;
+    end
+end
 out = out(:)';
